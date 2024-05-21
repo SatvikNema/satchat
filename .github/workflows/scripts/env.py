34fc43476f26e8bac41db1df69aab2c90d7cfb66
Satvik Nema <satvik.nema@salesforce.com>
2024-05-21 23:47:18 +0530

    correct target and source branches

--- .github/workflows/scripts/env.py
+++ .github/workflows/scripts/env.py
@@ -5,4 +5,4 @@ repo: str = os.environ['GITHUB_REPOSITORY']
 pull_number: int = int(os.environ['PR_NUMBER'])
 commit_hash: str = os.environ.get('GITHUB_SHA', "<HASH NOT AVAILABLE>")
 source_branch: str = os.environ['SOURCE_BRANCH']
-base_branch: str = os.environ['BASE_BRANCH']
+target_branch: str = os.environ['TARGET_BRANCH']
--- .github/workflows/scripts/my_file.py
+++ .github/workflows/scripts/my_file.py
@@ -1,6 +1,6 @@
 import patch_parser
 from code_analyser import analyse
-from env import base_branch, source_branch
+from env import target_branch, source_branch
 from github_client import gh_pull_request, gh_repository, gh_client_instance
 
 file_change_metadata = []
@@ -8,7 +8,7 @@ for file in gh_pull_request.get_files():
     filename = file.filename
     lines_changed = patch_parser.get_lines_changed(file.patch)
 
-    old_contents = gh_repository.get_contents(filename, ref=base_branch)
+    old_contents = gh_repository.get_contents(filename, ref=target_branch)
     new_contents = gh_repository.get_contents(filename, ref=source_branch)
 
     file_change_metadata.append({
