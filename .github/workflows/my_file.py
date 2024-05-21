8b6b08211ba79506cc97e2388a17dfe0e17bc64a
Satvik Nema <satvik.nema@salesforce.com>
2024-05-21 12:51:58 +0530

    leverage token to print all repos

--- .github/workflows/my_file.py
+++ .github/workflows/my_file.py
@@ -1,4 +1,11 @@
 import os
+from github import Github
+from github import Auth
 
-for key in os.environ.keys():
-    print(f'{key} -> {os.environ[key]}')
+auth = Auth.Token(os.environ['GITHUB_TOKEN'])
+g = Github(auth=auth)
+
+for repo in g.get_user().get_repos():
+    print(repo.name)
+
+g.close()
